6eb6699478a5f3a879d0be95e048211be2e9c279
easterwood <alexanderosterwald@gmx.de>
2022-01-14 08:33:35 +0100

    Initial wake up script

---
+++ wake-up-every-business-day-from.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Wake up PC every business day from specified state.
+#
+# This script selects the next business day from now and sets
+# a wake up timer. Afterwards it puts the PC into some sleep state.
+# The sleep state can be selected via the first argument. The default is
+# 'disk'. Supported sleep states see 'rtcwake -m'.
+
+DEFAULT_OFF_MODE="disk"
+
+mode=${1:-DEFAULT_OFF_MODE}
+dayIdx=1
+dayName=$(date +%a -d "now + $dayIdx days 6:00" | tr '[:lower:]' '[:upper:]')
+
+echo "Day of tomorrow $dayName"
+
+while [ "$dayName" == "SA" -o "$dayName" == "SO" ]
+do
+  dayIdx=$((dayIdx+1))
+  echo "Plus $dayIdx days"
+  dayName=$(date +%a -d "now + $dayIdx days 6:00" | tr '[:lower:]' '[:upper:]')
+  echo "Resulting day name $dayName"
+done
+
+if [ "$dayName" != "SA" -a "$dayName" != "SO" ]
+then
+    selected=$(date +"%Y-%m-%d %H:%M:%S" -d "now + $dayIdx days 6:00")
+    seconds=$(date -d"$selected" +%s)
+    echo "Restart PC at $selected (in seconds $seconds)"
+    /usr/sbin/rtcwake -m $DEFAULT_OFF_MODE -t $seconds
+else
+    echo "Wrong day calculated: $dayName"
+    exit 1
+fi
